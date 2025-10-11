75ddcf6949d5d7e2c0bb5131901d16f1a581c1f8
Kenny <kennylopez202031554@cunoc.edu.gt>
2025-10-11 00:51:59 -0600

    inicializando proyecto

---
+++ ecommerce-gt/src/main/java/com/ecommerce/gt/ecommerce_gt/EcommerceGtApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.gt.ecommerce_gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGtApplication.class, args);
+	}
+
+}
---
+++ ecommerce-gt/src/test/java/com/ecommerce/gt/ecommerce_gt/EcommerceGtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.gt.ecommerce_gt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
